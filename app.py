7f7216488ef80fdf22b988197c8dd4cfe6cd9a3b
lotta <lotta@skriva.net>
2025-08-18 13:40:14 +0200

    Introduced an error to test linting

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv ()
 def random_number_generator():
     secret = os.getenv('SUPER_SECRET_PHRASE')
     if secret == '1up':
